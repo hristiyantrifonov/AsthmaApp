791ce8b833f551348e80f2f25a620561bb53fc23
user136629 <user136629@EU238.local>
2018-04-12 02:38:33 +0100

    Doctors have separate side now +GMC Number input into the database + Basic layout to develop the doctors' interface

---
+++ MyAsthmaApp/MyAsthmaApp/GMCNumberSubmissionViewController.swift
@@ -0,0 +1,75 @@
+//
+//  GMCNumberSubmissionViewController.swift
+//  MyAsthmaApp
+//
+//  Created by user136629 on 4/12/18.
+//  Copyright © 2018 Hristiyan Trifonov. All rights reserved.
+//
+
+import UIKit
+import ZeroKit
+import Firebase
+
+class GMCNumberSubmissionViewController: UIViewController {
+
+    @IBOutlet weak var gmcNumberTextField: UITextField!
+    
+    var ref: DatabaseReference!
+    let userID = Auth.auth().currentUser?.uid
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        
+        print("User with ID: \(userID)")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func submitButtonClicked(_ sender: Any) {
+        print("Submission")
+        
+        let GMCNumber = gmcNumberTextField.text!
+        
+        if let userID = userID {
+            
+            //Get the user
+            ref.child("users").child(userID).observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                let userObject = snapshot.value as? NSDictionary
+                
+                //We set the GMC Number and we change the value of the initial login so that we do not
+                //get this page for GMC Number setup again
+                userObject?.setValue(GMCNumber, forKey: "GMC_Reference_Number")
+                userObject?.setValue(false, forKey: "First_Login")
+                
+                //Perform the change and save to the real-time database
+                self.ref.child("users").child(userID).setValue(userObject)
+                
+                self.performSegue(withIdentifier: "goToDoctorMain", sender: self)
+                
+            }) { (error) in
+                print(error.localizedDescription)
+                
+            }
+            
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyAsthmaApp/MyAsthmaApp/LoginViewController.swift
+++ MyAsthmaApp/MyAsthmaApp/LoginViewController.swift
@@ -16,11 +16,14 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet weak var errorLabel: UILabel!
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ref = Database.database().reference()
         errorLabel.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
 
@@ -39,6 +42,7 @@ class LoginViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!) {
             (user, error) in
             
+            
             if error != nil {
                 print(error!)
                 SVProgressHUD.dismiss()
@@ -49,20 +53,47 @@ class LoginViewController: UIViewController {
                 print("Login Successful!")
                 SVProgressHUD.dismiss()
                 
-                self.performSegue(withIdentifier: "goToRoot", sender: self)
+                print("JA")
+                print(user?.uid)
+        
+                
+                if let userID = user?.uid {
+                    
+                    //Finds the current user in the RealTime Database in Firebase and
+                    //obtains the users' values for displaying in the app
+                    self.ref.child("users").child(userID).observeSingleEvent(of: .value, with: { (snapshot) in
+                        
+                        let userObject = snapshot.value as? NSDictionary
+                        
+                        let userType = userObject?["User_Type"] as? String ?? ""
+                        let firstLogin = userObject?["First_Login"] as? Bool 
+                        
+                        print("DA")
+                        print(userType)
+                        
+                        if userType == "Doctor"{
+                            if firstLogin == true{
+                                self.performSegue(withIdentifier: "goToGMCSetup", sender: self)
+                            }else{
+                                self.performSegue(withIdentifier: "goToDoctorMain", sender: self)
+                            }
+                        }else{
+                            self.performSegue(withIdentifier: "goToRoot", sender: self)
+                        }
+                        
+                    }) { (error) in
+                        print(error.localizedDescription)
+                        
+                    }
+                    
+                }
+                
+//                self.performSegue(withIdentifier: "goToRoot", sender: self)
             }
             
+        
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
